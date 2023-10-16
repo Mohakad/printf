b7e016e41333c887a03b3571cad38321af4a3495
Mohakad <mohkadtrd@gmail.com>
2023-10-16 19:18:05 +0300

    task0

--- _printf.c
+++ _printf.c
@@ -16,7 +16,7 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
-	while (*format != '\0')
+	while (*format)
 	{
 		if (*format == '%')
 		{
---
+++ print_perc.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_perc- print %
+ * Return: number of char printed
+ */
+int print_perc(void)
+{
+	_putchar('%');
+	return (1);
+}
