469e32180b96fb12c45e7e1b5171f3b849e8afb8
Mohakad <mohkadtrd@gmail.com>
2023-10-18 10:31:00 -0700

    2.biner

--- main.h
+++ main.h
@@ -16,7 +16,7 @@ int print_integer(va_list number);
 int print_perc(void);
 int print_ch(va_list list);
 int _putchar(char c);
-int print_biner(unsigned int bine);
+int print_biner(va_list biner);
 int _printf(const char *format, ...);
 int print_str(va_list str);
 int str_len(char *str);
--- print_biner.c
+++ print_biner.c
@@ -1,18 +1,32 @@
 #include "main.h"
 /**
  * print_biner- print binery
- * @bine: argument
+ * @biner: argument
  * Return: counter
  */
-int print_biner(unsigned int bine)
+int print_biner(va_list biner)
 {
-	int len = 0;
+	int len = 0, flag = 0, a = 1, ind = 0, bine;
 
-	if (bine > 1)
+	unsigned int n = va_arg(biner, unsigned int), poin;
+
+	while (ind < 32)
+	{
+		poin = ((a << (32 - ind)) & n);
+		if (poin >> (31 - ind))
+			flag = 1;
+		if (flag & 1)
+		{
+			len++;
+			bine = poin >> (31 - ind);
+			_putchar(bine + 48);
+		}
+		ind++;
+	}
+	if (len == 0)
 	{
 		len++;
-		print_biner(bine >> 1);
+		_putchar('0');
 	}
-	_putchar((bine & 1) + '0');
 	return (len);
 }
